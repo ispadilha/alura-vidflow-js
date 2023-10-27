18ecdc9fec447f5c1fee122e3989c71ed5c5dc6b
ispadilha <israel.padilha@gmail.com>
2023-10-27 08:05:45 -0300

    Fetching and rendering the videos

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
+const videosContainer = document.querySelector(".videos__container")
+
 const api = fetch("http://localhost:3000/videos")
     // console.log(api)
     // .then((resposta) => {console.log(resposta)})
-    .then((resposta) => {console.log(resposta.json())})
+    // .then((resposta) => {console.log(resposta.json())})
+    .then((resposta) => resposta.json())
+    .then((videos) => {
+        videos.forEach((video) => {
+            videosContainer.innerHTML += `
+                <li class="videos__item">
+                    <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
+                    <div class="descricao-video">
+                        <img class="img-canal" src="${video.imagem} alt="Loogo do Canal">
+                        <h3 class="titulo-video">${video.titulo}</h3>
+                        <p class="titulo-canal">${video.descricao}</p>
+                    </div>
+                </li>
+            `
+        })
+    })
